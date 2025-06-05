9427e04f7d44a11bab4588f8a5908d134b5a14fe
Ivan <contaivanrodrigues@gmail.com>
2025-06-05 14:22:30 -0300

    feat: conect api strip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
 import { ProductType } from "@/types/ProductType";
 import Product from "./components/Productj";
+import Stripe from 'stripe';
 
-async function getProducts() {
-  const res = await fetch("https://fakestoreapi.com/products");
-  if (!res.ok) {
-    throw new Error("Falid to fetch data");
-  }
-  return res.json();
+async function getProducts(): Promise<ProductType[]> {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2025-05-28.basil',
+  });
+
+  const products = await stripe.products.list();
+  const formatedProducts = await Promise.all(
+    products.data.map(async (product) => {
+      const price = await stripe.prices.list({
+        product: product.id,
+      });
+      
+      return {
+        id: product.id,
+        price: price.data[0].unit_amount,
+        name: product.name,
+        image: product.images[0],
+        description: product.description,
+        currency: price.data[0].currency,
+      };
+    })
+  );
+
+
+  return formatedProducts;
 }
 
 export default async function Home() {
   const products = await getProducts();
+   console.log(products)
 
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
