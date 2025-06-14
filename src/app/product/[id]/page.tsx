d2f0ea7ff1a8db98470fe333c2521c876f2217ab
Ivan <contaivanrodrigues@gmail.com>
2025-06-13 21:18:45 -0300

    feat: clean Readme

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,15 +1,17 @@
-import AddCart from '@/app/components/AddCart';
-import ProductImage from '@/app/components/ProductImage';
-import { formatPrice } from '@/lib/utils';
-import Stripe from 'stripe';
+import AddCart from "@/app/components/AddCart";
+import ProductImage from "@/app/components/ProductImage";
+import { formatPrice } from "@/lib/utils";
+import Stripe from "stripe";
 
-type Props = {
-  params: { id: string };
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
 };
 
 async function getProduct(id: string) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-05-28.basil',
+   apiVersion: "2025-05-28.basil",
   });
   const produto = await stripe.products.retrieve(id);
   const price = await stripe.prices.list({
@@ -26,21 +28,23 @@ async function getProduct(id: string) {
   };
 }
 
-export default async function ProductPage({ params }: Props) {
-  const product = await getProduct(params.id);
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
 
   return (
-    <div className='flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 p-10'>
+    <div className="flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 p-10">
       <ProductImage product={product} />
-      <div className='flex flex-col'>
-        <div className='pb-4'>
-          <h1 className='text-2xl font-bold text-gray-300'>{product.name}</h1>
-          <h2 className='text-xl text-teal-600 font-bold'>
+      <div className="flex flex-col">
+        <div className="pb-4">
+          <h1 className="text-2xl font-bold text-gray-300">{product.name}</h1>
+          <h2 className="text-xl text-teal-600 font-bold">
             {formatPrice(product.price)}
           </h2>
         </div>
-        <div className='pb-4'>
-          <p className='text-sm text-gray-400'>{product.description}</p>
+        <div className="pb-4">
+          <p className="text-sm text-gray-400">{product.description}</p>
         </div>
         <AddCart product={product} />
       </div>
